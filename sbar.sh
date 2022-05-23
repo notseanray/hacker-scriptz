a49db4c3e8b3d745be2407ef7442461160423af5
NotCreative21 <seanray410@gmail.com>
2022-05-22 20:28:05 -0400

    update status bar script

--- sbar.sh
+++ sbar.sh
@@ -9,6 +9,10 @@ echo "$$" > /tmp/sbar
 update_cpu () { 
 	# sketchy way to calculate actual percent usage 
 	cpu_idle=`top -b -n 1 | grep Cpu | awk '{print $8}'|cut -f 1 -d "."`
+	# when 0% usage
+	if [[ $cpu_idle = "id," ]]; then
+		cpu_idle=100
+	fi
 	cpu=`expr 100 - $cpu_idle`
 }
 
@@ -21,8 +25,12 @@ update_time () {
 }
 
 update_weather () { 
-	weather="$(curl -s "wttr.in?format=1"| sed -E "s/^(.).*\+/\1/")" 
-	#weather="$(curl -s "wttr.in?format="%t"")"
+	weather_api="$(curl -s "wttr.in?format=1"| sed -E "s/^(.).*\+/\1/")" 
+	if [[ ${#weather_api} -gt 1 && ${#weather_api} -lt 10 ]]; then 
+		weather="$weather_api | "
+	else
+		weather=""
+	fi
 }
 
 update_vol () { 
@@ -30,25 +38,43 @@ update_vol () {
 	head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')"
 }
 
-btc () {
-  btc="$(curl -s http://api.coindesk.com/v1/bpi/currentprice.json | jq -r .bpi.USD.rate)"
+update_btc () {
+	btc_api="$(curl -s http://api.coindesk.com/v1/bpi/currentprice.json | jq -r .bpi.USD.rate)"
+	if [[ ${#btc_api} -gt 0 ]]; then
+		btc="₿ $btc_api |"
+	else 
+		btc=""
+	fi
 }
 # set initial value 
 update_vol
 
 display () { 
   #printf "%s\n" " $event [$weather] [$memory $cpu] [$bat] [$backlight] [$vol] $time "
-	xsetroot -name "₿ $btc | $weather | ⌨ $cpu% | ⚠ $mem | ^ $vol% | $time"
+	xsetroot -name "$btc $weather⌨ $cpu% | ⚠ $mem | ^ $vol% | $time"
 }
+
+update_btc
+update_weather
+
+iter=0
 while true
 do
-	sleep 5 & wait && { 
-		btc
+	sleep 3 & wait && { 
+		iter=$((iter + 1))
+		if [[ $(($iter % 20)) -eq 0 ]]; then
+			update_btc
+			update_weather
+		fi
+		if [[ $(($iter % 2)) -eq 0 ]]; then 
+			update_vol
+		fi
+		if [[ $((iter)) -gt 1000 ]]; then
+			iter=0
+		fi
 		update_time
 		update_cpu
 		update_memory
-		update_vol
-		update_weather
 		display
 	}
 done 
